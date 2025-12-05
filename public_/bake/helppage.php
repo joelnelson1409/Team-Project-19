bf69b1a79d428a08a524daf7e82eca4f45aa1fc2
joelnelson1409 <240389521@aston.ac.uk>
2025-12-05 02:24:03 +0000

    Update helppage.php

--- public_/bake/helppage.php
+++ public_/bake/helppage.php
@@ -21,3 +21,30 @@ session_start();
             <p class="tagline">Your home for all your bakes and cakes</p>
         </div>
     </div>
+
+       <nav class="main-nav">
+        <ul>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="products.php">Products</a></li>
+            <li><a href="loginpage.php">Login</a></li>
+            <li class="has-dropdown">
+                <a href="categories.php">Categories</a>
+                <ul class="dropdown">
+                    <li><a href="products.php?category=cakes">Cakes</a></li>
+                    <li><a href="products.php?category=cookies">Cookies</a></li>
+                    <li><a href="products.php?category=pastries">Pastries</a></li>
+                    <li><a href="products.php?category=bread">Bread</a></li>
+                </ul>
+            </li>
+            <li><a href="basket.php">Basket</a></li>
+            <li><a href="register.php">Register</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="help.php" class="active">Help</a></li>
+        </ul>
+    </nav>
+
+    <button id="theme-toggle" aria-label="Toggle light or dark mode">
+       Dark mode
+    </button>
+</header>
