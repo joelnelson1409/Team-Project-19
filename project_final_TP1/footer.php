bf1f3b30f26adfd29d6c902b760be71cfdeb994b
alitoo007 <alitobonjour@gmail.com>
2025-12-05 04:29:13 +0000

    Delete project_final_TP1/footer.php

--- project_final_TP1/footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_SESSION['userID'])) {
-    include __DIR__ . '/footer_1.php';      // logged-in footer
-} else {
-    include __DIR__ . '/footer_guest.php';  // guest footer
-}
-?>
